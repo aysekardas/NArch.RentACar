2725f86588e57fae688a687d4327969dc193eace
aysekardas <aysekardas7@gmail.com>
2024-02-26 18:57:14 +0300

    add brands rules

--- src/nArch.RentACar/Persistence/Contexts/BaseDbContext.cs
+++ src/nArch.RentACar/Persistence/Contexts/BaseDbContext.cs
@@ -15,6 +15,8 @@ public class BaseDbContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
 
+    public DbSet<Brand> Brands { get; set; }
+
     public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration)
         : base(dbContextOptions)
     {
@@ -23,6 +25,7 @@ public class BaseDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
 }
