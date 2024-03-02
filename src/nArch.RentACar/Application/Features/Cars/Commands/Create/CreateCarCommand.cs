f7f65b2cece37cfe0f7296cbb88569c81a2a6db3
aysekardas <aysekardas7@gmail.com>
2024-03-02 21:52:43 +0300

    add car commands

---
+++ src/nArch.RentACar/Application/Features/Cars/Commands/Create/CreateCarCommand.cs
@@ -0,0 +1,50 @@
+﻿using Application.Features.Cars.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Cars.Commands.Create;
+public class CreateCarCommand : IRequest<CreatedCarResponse>
+{
+    public Guid ColorId { get; set; }
+
+    public Guid ModelId { get; set; }
+
+    public string CarState { get; set; }
+
+    public string Kilometer { get; set; }
+
+    public short ModelYear { get; set; }
+
+    public string Plate { get; set; }
+
+
+    public class CreateCarCommandHandler : IRequestHandler<CreateCarCommand, CreatedCarResponse>
+    {
+        private readonly ICarRepository _carRepository;
+        private readonly IMapper _mapper;
+        private readonly CarBusinessRules _carBusinessRules;
+        public CreateCarCommandHandler(IMapper mapper, CarBusinessRules carBusinessRules, ICarRepository carRepository)
+        {
+            _mapper = mapper;
+            _carBusinessRules = carBusinessRules;
+            _carRepository = carRepository;
+        }
+
+        public async Task<CreatedCarResponse> Handle(CreateCarCommand request, CancellationToken cancellationToken)
+        {
+            Car car = _mapper.Map<Car>(request);
+
+            Car addedCar = await _carRepository.AddAsync(car);
+
+            CreatedCarResponse createdCarResponse = _mapper.Map<CreatedCarResponse>(addedCar);
+            return createdCarResponse;
+        }
+    }
+}
---
+++ src/nArch.RentACar/Application/Features/Cars/Commands/Create/CreateCarCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Cars.Commands.Create;
+public class CreateCarCommandValidator : AbstractValidator<CreateCarCommand>
+{
+    public CreateCarCommandValidator()
+    {
+        RuleFor(c => c.CarState).NotEmpty().NotNull();
+        RuleFor(c => c.ModelYear).NotEmpty().InclusiveBetween((short)1880, short.MaxValue);
+        RuleFor(c=> c.CarState).NotEmpty().NotNull();
+        RuleFor(c => c.Kilometer).NotEmpty().NotNull().Must(str => int.TryParse(str, out _));
+        RuleFor(c => c.Plate).NotNull().NotEmpty();
+
+            //.GreaterThanOrEqualTo(1880);
+    }
+}
