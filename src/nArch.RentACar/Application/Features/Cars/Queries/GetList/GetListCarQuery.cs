83219406ab56d22bc11478ea12ecffb33ef23e8b
aysekardas <aysekardas7@gmail.com>
2024-03-02 21:53:38 +0300

    add car rules

---
+++ src/nArch.RentACar/Application/Features/Cars/Queries/GetList/GetListCarQuery.cs
@@ -0,0 +1,37 @@
+﻿using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using NArchitecture.Core.Application.Requests;
+using NArchitecture.Core.Application.Responses;
+using NArchitecture.Core.Persistence.Paging;
+
+
+namespace Application.Features.Cars.Queries.GetList;
+public class GetListCarQuery : IRequest<GetListResponse<GetListCarItemDto>>
+{
+
+    public PageRequest PageRequest { get; set; }
+
+    public class GetListCarQueryHandler : IRequestHandler<GetListCarQuery, GetListResponse<GetListCarItemDto>>
+    {
+        private readonly ICarRepository _carRepository;
+        private readonly IMapper _mapper;
+
+        public GetListCarQueryHandler(ICarRepository CarRepository, IMapper mapper)
+        {
+            _carRepository = CarRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<GetListResponse<GetListCarItemDto>> Handle(GetListCarQuery request, CancellationToken cancellationToken)
+        {
+            IPaginate<Car> Cars = await _carRepository.GetListAsync(index: request.PageRequest.PageIndex, size: request.PageRequest.PageSize);
+
+            GetListResponse<GetListCarItemDto> response = _mapper.Map<GetListResponse<GetListCarItemDto>>(Cars);
+
+            return response;
+        }
+    }
+}
+
---
+++ src/nArch.RentACar/Application/Features/Cars/Rules/CarBusinessRules.cs
@@ -0,0 +1,31 @@
+﻿using Application.Services.Repositories;
+using Domain.Entities;
+using NArchitecture.Core.Application.Rules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Cars.Rules;
+public class CarBusinessRules : BaseBusinessRules
+{
+    private readonly ICarRepository _carRepository;
+
+    public CarBusinessRules(ICarRepository carRepository)
+    {
+        _carRepository = carRepository;
+    }
+
+    //TODO
+    //public async Task KilometerBeNotNegative(string kilometer)
+    //{
+    //    Car? carKilometer = await _carRepository.GetAsync()
+    //}
+
+    //public async Task ModelYearRange(short modelYear)
+
+    //{
+    //   throw new NotImplementedException();
+    //}
+}
